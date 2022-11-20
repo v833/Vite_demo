c2634442d9de64929121c77a57bf8b8418a827a7
v833 <2507301541@qq.com>
2022-11-20 15:46:40 +0800

    feat: hmr

--- hmr-demo/hmr/main.js
+++ hmr-demo/hmr/main.js
@@ -1,26 +1,40 @@
-/*
- * @Description:
- * @Author: v833
- * @Date: 2022-08-13 23:09:45
- * @LastEditors: v833
- * @LastEditTime: 2022-08-14 11:26:37
- */
 import './style.css'
 
 // 将渲染改成函数
-
+let index = import.meta.hot.data?.cache?.getIndex?.() || 0
+let timer = null
 export function render() {
-  document.querySelector('#app').innerHTML = `
+  timer = setInterval(() => {
+    document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Hello Vite!</h1>
+    <h1>Hello Vite!!${index++}</h1>
   </div>
 `
+  }, 1000)
 }
 
 render()
 
+// side effect
+// const timer = setInterval(() => {
+//   console.log(index++)
+// }, 1000)
+
+// 打包时会tree shaking
 if (import.meta.hot) {
   import.meta.hot.accept((newMoudle) => {
+    console.log('newMoudle: ', newMoudle)
     newMoudle.render()
   })
+  import.meta.hot.dispose(() => {
+    console.log('dispose')
+    if (timer) {
+      clearInterval(timer)
+    }
+  })
+  import.meta.hot.data.cache = {
+    getIndex() {
+      return index
+    }
+  }
 }
